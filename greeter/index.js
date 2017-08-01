c70a91eb318e5045cc576a0b13a007a2025ba854
td2014 <anthonyd2004@gmail.com>
2017-08-01 12:09:20 -0700

    Added French

--- greeter/index.js
+++ greeter/index.js
@@ -2,6 +2,7 @@
 var APP_ID = 'amzn1.ask.skill.50996551-2248-4aa8-859e-6f388b2cb978';
 var AlexaSkill = require('./AlexaSkill');
 var SPEECH_OUTPUT = 'Hello';
+var SPEECH_OUTPUT_FRENCH = 'Bonjour. cawhm mohn saah vaah';
 
 var GreeterService = function() {
     AlexaSkill.call(this, APP_ID);
@@ -11,10 +12,14 @@ GreeterService.prototype = Object.create(AlexaSkill.prototype);
 var helloResponseFunction = function(intent, session, response) {
     response.tell(SPEECH_OUTPUT);
 };
+var helloResponseFrenchFunction = function(intent, session, response) {
+    response.tell(SPEECH_OUTPUT_FRENCH);
+};
 GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
 
 GreeterService.prototype.intentHandlers = {
-    'HelloWorldIntent': helloResponseFunction
+    'HelloWorldIntent': helloResponseFunction,
+    'HelloWorldFrenchIntent': helloResponseFrenchFunction
 };
 
 // This is a AWS Lambda Handler.  Handles JSON messages from the interface and routes them
