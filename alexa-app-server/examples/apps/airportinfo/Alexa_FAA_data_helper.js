f8ede243ccdd5d6c86fc29bd0c8506c06c7adc5f
td2014 <anthonyd2004@gmail.com>
2017-08-02 11:15:44 -0700

    Some cleanup from older versions

--- alexa-app-server/examples/apps/airportinfo/Alexa_FAA_data_helper.js
+++ alexa-app-server/examples/apps/airportinfo/Alexa_FAA_data_helper.js
@@ -5,49 +5,6 @@ var ENDPOINT = 'http://services.faa.gov/airport/status/';
 
 function AlexaFAADataHelper() {}
 
-AlexaFAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
-    var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.') ({
-        weather: airportStatus.weather.weather,
-        temp: airportStatus.weather.temp,
-        wind: airportStatus.weather.wind
-    });
-
-    if (airportStatus.delay === 'true') {
-        var template = _.template('There is currently a delay for ${airport}. ' +
-            'The average delay time is ${delay_time}. ' +
-            'Delay is because of the following: ${delay_reason}. ${weather}');
-
-        return template({
-      
-            airport: airportStatus.name,
-            delay_time: airportStatus.status.avgDelay,
-            delay_reason: airportStatus.status.reason,
-            weather: weather
-        });
-
-        } else {
-
-          // no delay
-
-          return _.template('There is currently no delay at ${airport}. ${weather}')({
-              airport: airportStatus.name,
-              weather: weather
-          });
-
-          }
-
-          };
-
-
-AlexaFAADataHelper.prototype.requestAirportStatus = function(airportCode) {
-    return this.getAirportStatus(airportCode).then(
-        function(response) {
-            console.log('success - received airport info for ' + airportCode);
-            return response.body;
-        }
-    );
-};
-
 AlexaFAADataHelper.prototype.getAirportStatus = 
 function(airportCode) {
     var options = {
@@ -57,6 +14,39 @@ function(airportCode) {
     };
 
     return requestPromise(options);
+};
+
+AlexaFAADataHelper.prototype.formatAirportStatus = function(airportStatusObject) {
+
+    if (airportStatusObject.delay === 'true') {
+        var template = _.template('There is currently a delay for ${airport}. ' +
+            'The average delay time is ${delay_time}. ' +
+            'Delay is because of the following: ${delay_reason}.');
+        return template({
+            airport: airportStatusObject.name,
+            delay_time: airportStatusObject.status.avgDelay,
+            delay_reason: airportStatusObject.status.reason,
+        });
+        } else {
+
+          // no delay
+
+          return _.template('There is currently no delay at ${airport}.')({
+              airport: airportStatusObject.name,
+          });
+
+        }
+
+    };
+
+
+AlexaFAADataHelper.prototype.requestAirportStatus = function(airportCode) {
+    return this.getAirportStatus(airportCode).then(
+        function(response) {
+            console.log('success - received airport info for ' + airportCode);
+            return response.body;
+        }
+    );
 };
 
 module.exports = AlexaFAADataHelper;
