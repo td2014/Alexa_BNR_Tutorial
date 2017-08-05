c85e55c7ef0238d60171c9f44a9f29c000d2c3fa
td2014 <anthonyd2004@gmail.com>
2017-08-05 09:37:28 -0700

    Added states to madlibbuilder index.js

--- alexa-app-server/examples/apps/madlibbuilder/index.js
+++ alexa-app-server/examples/apps/madlibbuilder/index.js
@@ -4,28 +4,54 @@ var _ = require('lodash');
 var Skill = require('alexa-app');
 var SkillService = new Skill.app('madlibbuilder');
 
-SkillService.launch(function(request, response) {
-    var prompt = 'Welcome to Madlibs. ' 
-        + 'To create a new madLib, say create a madlib';
-    
-    var myWordArray = new Array();
-    myWordArray.push('Word1');
-    
-    response.sessionObject.set('WordArray', myWordArray); 
-    console.log('SessionObject = ', response.sessionObject); 
-    response.say(prompt).shouldEndSession(false);
-
-});
-
-
-SkillService.intent('AddWordIntent', {},
+SkillService.intent('AMAZON.HelpIntent', {}, 
     function(request, response) {
-        console.log('intent request: request = ', request);
-        var myWordArray = request.sessionAttributes.WordArray;
-        console.log('intent request: myWordArray = ', myWordArray);
-        myWordArray.push('Word2');
+        var prompt = 'How may I help you?'
+        response.say(prompt).shouldEndSession(false);
+    });
+
+SkillService.launch(
+    function(request, response) {
+        var prompt = 'Welcome to Madlibs. ' 
+            + 'To create a new madLib, say create a madlib';
+    
+        var myWordArray = new Array();
+    
         response.sessionObject.set('WordArray', myWordArray); 
-        response.say('What').shouldEndSession(false);
-});
+        response.sessionObject.set('AppState', 'ChooseMadlib'); 
+        response.say(prompt).shouldEndSession(false);
+
+    });
+
+SkillService.intent('FillIntent', {}, 
+    function(request, response) {
+        response.sessionObject.set('AppState', 'FillingMadlib'); 
+    });
+
+SkillService.intent('WordIntent', {},
+    function(request, response) {
+        
+        var myWordArray = request.sessionAttributes.WordArray;
+        myWordArray.push('Word2');
+
+        response.sessionObject.set('WordArray', myWordArray); 
+        response.sessionObject.set('AppState', 'DoneMadlib'); 
+        response.say('What?').shouldEndSession(false);
+    });
+
+SkillService.intent('ReadbackIntent', {}, 
+    function(request, response) {
+        response.sessionObject.set('AppState', 'End'); 
+    });
+
+SkillService.intent('PlayAgainIntent', {}, 
+    function(request, response) {
+        response.sessionObject.set('AppState', 'ChooseMadlib'); 
+    });
+
+SkillService.intent('EndGameIntent', {}, 
+    function(request, response) {
+        response.sessionObject.set('AppState', 'End'); 
+    });
 
 module.exports = SkillService;
