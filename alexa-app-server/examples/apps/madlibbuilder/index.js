90bbad0a31e97506698a2cee31450c0565dd0cfd
td2014 <anthonyd2004@gmail.com>
2017-08-04 19:09:22 -0700

    Fixed session setting.

---
+++ alexa-app-server/examples/apps/madlibbuilder/index.js
@@ -0,0 +1,27 @@
+'use strict';
+module.change_code = 1;
+var _ = require('lodash');
+var Skill = require('alexa-app');
+var SkillService = new Skill.app('madlibbuilder');
+
+SkillService.launch(function(request, response) {
+    var prompt = 'Welcome to Madlibs. ' 
+        + 'To create a new madLib, say create a madlib';
+    
+    var myWordArray = new Array();
+    myWordArray.push('Word1');
+    
+    response.sessionObject.set('WordArray', myWordArray); 
+    console.log('SessionObject = ', response.sessionObject); 
+    response.say(prompt).shouldEndSession(false);
+
+});
+
+
+SkillService.intent('AddWordIntent', {},
+    function(request, response) {
+        console.log('launch request: request = ', request);
+        response.say('What').shouldEndSession(false);
+});
+
+module.exports = SkillService;
