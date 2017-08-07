810ff9afc9236df12d739e4b5495f088d6bb016c
td2014 <anthonyd2004@gmail.com>
2017-08-07 11:19:17 -0700

    Added madlib skeletons

---
+++ alexa-app-server/examples/apps/madlibbuilder/madlibStories.js
@@ -0,0 +1,8 @@
+// A collection of Madlib stories that I made up.
+module.exports = {
+    "MADLIB_NAMES" : ["November", "Summertime", "Trees"],
+    "MADLIB_CONTENT" : ["One upon a November, there was a happy {noun}.",
+                        "Once upon a summertime, there was {adj} bicycle which looked like a big {noun}.",
+                        "Once upon a forest, there was this {adj} tree, next to a gigantic {noun}."
+                       ]
+} 
