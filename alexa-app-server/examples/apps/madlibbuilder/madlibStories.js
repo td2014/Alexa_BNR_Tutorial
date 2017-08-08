a74ca94bdcc00f64c216d58491060ec04e25efa6
td2014 <anthonyd2004@gmail.com>
2017-08-08 11:39:23 -0700

    Regex correct except for first word

--- alexa-app-server/examples/apps/madlibbuilder/madlibStories.js
+++ alexa-app-server/examples/apps/madlibbuilder/madlibStories.js
@@ -2,7 +2,7 @@
 module.exports = {
     "MADLIB_NAMES" : ["November", "Summertime", "Trees"],
     "MADLIB_CONTENT" : ["One upon a November, there was a happy {noun}.",
-                        "Once upon a summertime, there was {adj} bicycle which looked like a big {noun}.",
+                        "Once upon a summertime, there was this {adj} bicycle {pad which looked rsq} like a big {noun}.",
                         "Once upon a forest, there was this {adj} tree, next to a gigantic {noun}."
                        ]
 } 
--- alexa-app-server/examples/apps/madlibbuilder/my_madlib.js
+++ alexa-app-server/examples/apps/madlibbuilder/my_madlib.js
@@ -24,8 +24,15 @@ MyMadlib.prototype.get_full_story_spoken = function(wordArray){
 MyMadlib.prototype.set_story_template = function(storyTemplate){
 
 // Transform the storyTemplate into something than can be modified via lodash templates.
+// The below regex filters for text:  {keyword} or .  or , or words after a space or the first
+// word in the string. Attempts to filter out malformed keywords like {word  (no closing right curly brace.
+// Allowable:  
+//    {keyword}
+//    Word.  OR Word, OR Word (space)  -Any where.
+//    {abc. OR {abc, OR {abc(space)   or  abc}  NOT allowed
 
-    var wordArray = _.words(storyTemplate, /[a-zA-Z\{\}]+/g); 
+    var wordArray = _.words(storyTemplate, /[,.]|\{[a-zA-Z]+\}|\s[a-zA-Z]+(?!\})(?=\s|\.|\,)/g);
+
     console.log(wordArray);
     var storyTemplateObject = { 'name':'storyTemplate',
                                 'content':storyTemplate };
