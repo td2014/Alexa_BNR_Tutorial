a62eef9e91e189b9ac89d07416ffc2000d6e911a
td2014 <anthonyd2004@gmail.com>
2017-08-08 12:32:32 -0700

    First cut of keyword expander implemented

--- alexa-app-server/examples/apps/madlibbuilder/my_madlib.js
+++ alexa-app-server/examples/apps/madlibbuilder/my_madlib.js
@@ -34,6 +34,42 @@ MyMadlib.prototype.set_story_template = function(storyTemplate){
     var wordArray = _.words(storyTemplate, /[,.]|\{[a-zA-Z]+\}|\s[a-zA-Z]+(?!\})(?=\s|\.|\,)|^[a-zA-Z]+(?!\})(?=\s|\.|\,)/g);
 
     console.log(wordArray);
+
+    var counter = 0;
+    var wordFillTypeArray = new Array();
+    var storyArray = new Array();
+    var templateFillMap = new Map();
+    var recognizedWordTypes = require('./recognizedWordTypes.js');  
+    console.log('recognizedWordTypes = ', recognizedWordTypes);
+    console.log('recognizedWordTypes("adj") = ', recognizedWordTypes.TYPES['adj']);
+ 
+    for (let iWord of wordArray) {
+        console.log('iWord = ', iWord);
+        if (iWord.charAt(0)==="{" && iWord.charAt(iWord.length-1)==="}") {  //Keyword detected.
+       
+            try {
+                let expandedType = recognizedWordTypes.TYPES[iWord.substring(1,iWord.length-1)];
+                console.log('iWord.substring(1,iWord.length) = ', iWord.substring(1,iWord.length-1)); 
+                console.log('ExpandedType = ', expandedType);
+                wordFillTypeArray.push(expandedType);
+                 
+//                storyArray.push(iWordRep + ' ');
+//                templateFillMap.set(iWordRep, null);
+                counter+=1;  
+            } catch(err) {
+
+                console.log('Error.  Word type currently supported: ', iWord);
+            }     
+        } else {
+            storyArray.push(iWord+' ')
+        }
+       
+
+    }
+    console.log('storyArray = ', storyArray);
+    console.log('wordFillTypeArray = ', wordFillTypeArray);
+    console.log('templateFillMap = ', templateFillMap);
+
     var storyTemplateObject = { 'name':'storyTemplate',
                                 'content':storyTemplate };
     this.add_object(storyTemplateObject);
---
+++ alexa-app-server/examples/apps/madlibbuilder/recognizedWordTypes.js
@@ -0,0 +1,4 @@
+// A collection of Madlib stories that I made up.
+module.exports = {
+    "TYPES" : { "noun" : "noun", "adj":"adjective", "verb":"verb", "adv":"adverb"}
+} 
